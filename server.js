337bd8cbe2338567282d66454a70794fe0dee86a
Ivanildo Lopes da Silva Junior <ivanildo.lopes.jr@gmail.com>
2020-02-22 17:46:47 -0300

    Iniciando alterações no desafio

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+//const videos = require("./data")
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+});
+
+server.get('/', function(req, res){
+    return res.render('home');
+});
+
+server.get('/about', function(req, res){
+    return res.render('about');
+});
+
+server.get('/video', function(req, res){
+    const id = req.query.id;
+
+    const video = videos.find(function(video){
+        return video.id == id;
+    });
+
+    if (!video){
+        return res.send("Video not found!");
+    }
+
+    return res.render("video", { item: video });
+});
+
+server.listen(5000, function(){
+    console.log('server is running');
+});
